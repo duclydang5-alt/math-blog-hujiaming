42ab3a552b8a9bca5ecadae8a1cd24b9b249cc7b
hujia <3225444130@qq.com>
2026-01-02 19:05:29 +0800

    恢复首页index文件

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "duclydang5-alt.github.io/math-blog-hujiaming",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
